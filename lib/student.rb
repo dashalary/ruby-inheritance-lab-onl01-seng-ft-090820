c5acfe3b2faced30f6c0130d4e4a0e18328c9108
Dasha Lary <dk2800@nyu.edu>
2020-09-24 00:43:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,16 +1,16 @@
 class Student < User
-attr_accessor :knowledge
+# attr_accessor :knowledge
 
-def initialize(knowledge)
-  @knowledge = knowledge
-  knowledge = []
-end
+# def initialize(knowledge)
+#   @knowledge = knowledge
+#   knowledge = []
+# end
 
-# def learn
-#   @knowledge << "string"
-# end 
+# # def learn
+# #   @knowledge << "string"
+# # end 
 
-def knowledge 
-  @knowledge 
-end
-end
+# def knowledge 
+#   @knowledge 
+# end
+# end
